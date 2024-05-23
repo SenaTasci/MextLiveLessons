40b2913de183976e4365d8ce8e48f77618269c91
SenaTasci <tascisena99@gmail.com>
2024-05-23 09:25:56 +0300

    Update new project

---
+++ CustifyPro/Custify.API/Controllers/CustomersController.cs
@@ -0,0 +1,70 @@
+﻿using Custify.Domain.Dtos;
+using Custify.Domain.Entities;
+using Custify.Domain.Enums;
+using Custify.Domain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Custify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        CustomerService customerService = new();
+            
+        [HttpPost]
+
+        public void Add(Customer customer)
+        {
+            customerService.Add(customer);
+        }
+
+        [HttpGet]
+
+        public List<GetCustomerDto> GetAll()
+        {
+            var customers = customerService.GetAll()
+            .Select(c => new GetCustomerDto()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Surname = c.Surname,
+                Email = c.Email,
+                Gender = c.Gender,
+            }).ToList();
+
+            foreach (var customer in customers)
+            {
+              if (customer.Gender == Gender.Male)
+                    customer.GenderText = "Male";
+
+              else if (customer.Gender == Gender.Female)
+                    customer.GenderText = "Female";
+
+                else if (customer.Gender == Gender.PreferNotToSay)
+                    customer.GenderText = "PreferNotToSay";
+
+                else if (customer.Gender == Gender.Unknown)
+                    customer.GenderText = "Unknown";
+
+             
+
+            }
+            return customers;
+        }
+
+        [HttpPut]
+        public void Update(Customer customer)
+        {
+            customerService.Update(customer);
+        }
+
+        [HttpDelete]
+        public void Delete(Guid id)
+        {
+            customerService.Delete(id);
+        }
+
+    }
+}
---
+++ CustifyPro/Custify.Domain/Services/CustomerService.cs
@@ -0,0 +1,66 @@
+﻿using Custify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custify.Domain.Services
+{
+    public class CustomerService
+    {
+        private static List<Customer> customers = new();
+        public List<Customer> GetAll()
+        {
+            return customers;
+        }
+
+        public void Add(Customer customer)
+        {
+            
+            customers.Add(customer);
+        }
+
+        public void Update(Customer updateCustomer)
+        {
+           
+            foreach (var customer in customers)
+            {
+              
+                if  (customer.Id == updateCustomer.Id)
+                
+                {
+                    customer.Name = updateCustomer.Name;
+                    customer.Surname = updateCustomer.Surname;
+                    customer.Email = updateCustomer.Email;
+                    customer.PhoneNumber = updateCustomer.PhoneNumber;
+                    customer.Address = updateCustomer.Address;
+                    break;
+                }
+            
+            }
+        }
+
+
+        public void Delete(Guid id)
+
+        {
+            foreach (var customer in customers)
+            {
+
+                if (customer.Id == id)
+
+                {
+                   customers.Remove(customer);
+                    break;
+                }
+
+            }
+        }
+    }
+
+}
+
+
+
+
