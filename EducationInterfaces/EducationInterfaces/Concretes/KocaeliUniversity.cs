3e7e3a1e2090e9cb39aa05c3f29be403ad2406f3
SenaTasci <tascisena99@gmail.com>
2024-06-03 12:26:50 +0300

    Update

---
+++ EducationInterfaces/EducationInterfaces/Concretes/KocaeliUniversity.cs
@@ -0,0 +1,24 @@
+﻿using EducationInterfaces.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationInterfaces.Concretes
+{
+    public class KocaeliUniversity : IGradeCalculation
+    {
+
+        public double Grade1 { get; set; }
+
+        public double Grade2 { get; set; }
+
+        public double CalculateGrade()
+        {
+            return 0.4 * Grade1 + 0.6 * Grade2;
+        }
+    }
+
+
+}
---
+++ EducationInterfaces/EducationInterfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using EducationInterfaces.Concretes;
+
+KocaeliUniversity kocaeliUniversity = new KocaeliUniversity();
+
+kocaeliUniversity.Grade1 = 100;
+
+kocaeliUniversity.Grade2 = 90;
+
+double calculateGrade = kocaeliUniversity.CalculateGrade();
+
+Console.WriteLine($" Kocaeli Üniversitesi : {calculateGrade}");
+
+GebzeTeknik gebzeTeknik = new GebzeTeknik();
+
+gebzeTeknik.Grade1 = 70;
+
+gebzeTeknik.Grade2 = 80;
+
+double calculateGrade2 = gebzeTeknik.CalculateGrade();
+
+Console.WriteLine($" Gebze Teknik : {calculateGrade2}");
